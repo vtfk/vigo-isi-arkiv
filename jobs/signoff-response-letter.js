fdd8f6fe9256844466b2f9d11ff0be143beac0a4
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2023-12-03 15:46:56 +0100

    whops, nesten blemme der

--- jobs/signoff-response-letter.js
+++ jobs/signoff-response-letter.js
@@ -1,44 +1,38 @@
+const { callArchive } = require('../lib/call-archive')
 const { logger } = require('@vtfk/logger')
-const axios = require('../lib/axios-instance').getAxiosInstance()
-const { statisticsConfig } = require('../config')
-const { name, version } = require('../package.json')
 
 /**
  *
  * @param {import('../lib/typedefs').DocumentData} documentData
  */
-const stats = async (documentData) => {
-  logger('info', ['stats', 'Creating statistics element'])
+const signOffResponseLetter = async (documentData) => {
+  logger('info', ['signOffResponseLetter', 'Trying to signoff reponse-letter'])
 
-  const statisticsPayload = {
-    system: 'publish-teams-document', // Required. System name. New system creates a new collection
-    engine: `${name} ${version}`,
-    company: 'ORG', // Required. Sector
-    department: 'Kommunikasjon', // Optional. If missing, company will be set here.
-    description: 'Publisering av et dokumnent fra Sharepoint til enten Innsida eller nettsiden, eller begge deler', // Required. A description of what the statistic element represents
-    type: 'published teams document', // Required. A short searchable type-name that distinguishes the statistic element
-    externalId: `${documentData.webUrl}?web=1`, // Optional. ID in the external {system}
-    // optional fields:
-    siteName: documentData.libraryConfig.siteName,
-    libraryName: documentData.libraryConfig.libraryName,
-    tenantName: documentData.libraryConfig.tenantName,
-    publishedVersion: getCorrectPublishedVersionNumber(getDriveItemDataResult.versionNumberToPublish, documentData.libraryConfig.hasVersioning)
+  if (documentData.flowStatus.archiveResponseLetter.result.invalidAddress) {
+    logger('info', ['signOffResponseLetter', 'Either address block or invalid address, response letter has not been created, and will not signoff'])
+    return {
+      message: 'Either address block or invalid address, response letter has not been created, original document will not be signed off',
+      signOffResponse: null
+    }
   }
-  if (documentData.libraryConfig.innsidaPublishing && documentData.flowStatus.publishToInnsida?.result) {
-    statisticsPayload.InnsidaUrl = documentData.flowStatus.publishToInnsida.result.webUrl
-  } else {
-    statisticsPayload.InnsidaUrl = false
+
+  logger('info', ['signOffResponseLetter', 'Address is ok and letter is sent, signing off original document with BU (besvart utgående)'])
+  const signOffPayload = {
+    service: 'DocumentService',
+    method: 'SignOffDocument',
+    parameter: {
+      Document: documentData.flowStatus.archive.result.DocumentNumber,
+      ResponseCode: 'BU',
+      ReplyDocument: documentData.flowStatus.archiveResponseLetter.result.archiveResponse.DocumentNumber
+    }
   }
-  if (documentData.libraryConfig.webPublishing && documentData.flowStatus.publishToWeb?.result) {
-    statisticsPayload.nettsideUrl = documentData.flowStatus.publishToWeb.result.webUrl
-  } else {
-    statisticsPayload.nettsideUrl = false
+  const signOffResponse = await callArchive(documentData.flowStatus.county, 'archive', signOffPayload)
+  logger('info', ['signOffResponseLetter', 'Successfully signed off original document'])
+
+  return {
+    message: 'Successfully signed off original document',
+    signOffResponse
   }
-  
-  logger('info', ['stats', 'Posting stats to database'])
-  const { data } = await axios.post(statisticsConfig.url, statisticsPayload, { headers: { 'x-functions-key': statisticsConfig.key } })
-  logger('info', ['stats', 'Stats successfully created, great success'])
-  return data
 }
 
-module.exports = { stats }
+module.exports = { signOffResponseLetter }
